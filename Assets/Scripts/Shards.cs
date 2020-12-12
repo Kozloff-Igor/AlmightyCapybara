834920469745bc8846a31a7e9fe9fb0371673ec7
fortey <fortey@inbox.ru>
2020-12-12 21:34:09 +0700

    added star on UI

---
+++ Assets/Scripts/Shards.cs
@@ -0,0 +1,60 @@
+﻿using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shards : MonoBehaviour
+{
+    public static Shards instance = null;
+
+    private int numberOfUsed;
+    public int NumberOfUsed
+    {
+        get => numberOfUsed;
+        set
+        {
+            numberOfUsed = Mathf.Clamp(value, 0, 11);
+            SetShards(numberOfUsed, UsedParent);
+        }
+    }
+    private int numberOfUnused;
+    public int NumberOfUnused
+    {
+        get => numberOfUnused;
+        set
+        {
+            numberOfUsed = Mathf.Clamp(value, 0, 11);
+            SetShards(numberOfUnused, UnusedParent);
+        }
+    }
+
+    [SerializeField]
+    private Transform UnusedParent;
+
+    [SerializeField]
+    private Transform UsedParent;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+
+    public void SetShards(int value, Transform parent)
+    {
+        for (var i = 0; i <= parent.childCount; i++)
+        {
+            var child = parent.GetChild(i).GetComponent<Star>();
+            child.gameObject.SetActive(i < value);
+        }
+    }
+}
