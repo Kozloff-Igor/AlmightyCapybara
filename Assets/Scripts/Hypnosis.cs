b52e9a5333f01723515895862ecc7e6edf83867b
fortey <fortey@inbox.ru>
2020-12-13 15:06:59 +0700

    player without circle collider

--- Assets/Scripts/Hypnosis.cs
+++ Assets/Scripts/Hypnosis.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Hypnosis : MonoBehaviour
 {
+    List<Mouse> allMouses;
     List<Mouse> candidates = new List<Mouse>();
+    Transform mTransform;
 
     public GameObject signal;
+    public float hypnosisDistance = 4f;
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        var mouse = collision.GetComponent<Mouse>();
-        if (mouse && mouse.mouseType == Mouse.MouseType.free)
-        {
-            candidates.Add(mouse);
-        }
+        mTransform = transform;
+        allMouses = FindObjectsOfType<Mouse>().Where(m => m.mouseType == Mouse.MouseType.free).ToList();
     }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    var mouse = collision.GetComponent<Mouse>();
+    //    if (mouse && mouse.mouseType == Mouse.MouseType.free)
+    //    {
+    //        candidates.Add(mouse);
+    //    }
+    //}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        var mouse = collision.GetComponent<Mouse>();
-        if (mouse && candidates.Contains(mouse))
-        {
-            candidates.Remove(mouse);
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    var mouse = collision.GetComponent<Mouse>();
+    //    if (mouse && candidates.Contains(mouse))
+    //    {
+    //        candidates.Remove(mouse);
+    //    }
+    //}
 
     public Mouse Hypnotize()
     {
@@ -42,6 +50,19 @@ public class Hypnosis : MonoBehaviour
 
     private void FixedUpdate()
     {
+        foreach(var mouse in allMouses)
+        {
+            if(mouse.mouseType==Mouse.MouseType.free && Vector3.Distance(mouse.transform.position, mTransform.position) < hypnosisDistance)
+            {
+                if (!candidates.Contains(mouse))
+                    candidates.Add(mouse);
+            }
+            else if(candidates.Contains(mouse))
+            {
+                candidates.Remove(mouse);
+            }
+        }
+
         signal.SetActive(candidates.Count > 0 && Shards.instance.NumberOfUnused>Shards.instance.NumberOfUsed);
     }
 }
